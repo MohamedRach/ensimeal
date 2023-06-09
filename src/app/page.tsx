ac2ee52bd5aab154a13219135f215133bfe9ce79
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-06-09 16:35:58 +0100

    fix a bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const Home = () => {
         <div className = "meal-result">
                 <h2 className = "title">Your Recipes:</h2>
         <Suspense fallback = {<LoadingSkeleton />}>
-          {/* @ts-expect-error Async Server Component */}
+          
           <Meal></Meal>
         </Suspense>
         </div>
